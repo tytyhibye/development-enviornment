017f96e265e2cac9fe14f6053d72a8902a2189b3
Tyler Bates <Fondlytyler@gmail.com>
2020-04-21 18:09:47 -0700

    update my environment for optimization.

--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,54 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-entry: './src/main.js',
-output: {
-  filename: 'bundle.js',
-  path: path.resolve(__dirname, 'dist')
-},
-devtool: 'eval-source-map',
-devServer: {
-  contentBase: './dist'
-},
-plugins: [
-  new UglifyJsPlugin({ sourceMap: true }),
-  new CleanWebpackPlugin(),
-  new Dotenv(),
-  new HtmlWebpackPlugin({
-    title: 'Ping Pong',
-    template: './src/index.html',
-    inject: 'body'
-  })
-],
-module: {
-  rules: [
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    },
-    {
-      test: /\.js$/,
-      exclude: [
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      title: '',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
           /node_modules/,
           /spec/
         ],
-      loader: "eslint-loader"
-    }
-  ]
-}
+        loader: "eslint-loader"
+      }
+    ]
+  }
 };
